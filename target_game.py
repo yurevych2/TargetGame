49c7287767c67a33aa401aaf5a441237e96097f0
Yurevych <yurevych.pn@ucu.edu.ua>
2022-11-04 14:23:22 +0200

    Created the functions to get num of repetitions of letters in a word and check requirements

--- target_game.py
+++ target_game.py
@@ -8,12 +8,34 @@ def get_num_of_letters(word):
     '''
     Return [('h', 1), ('e', 1), ('o', 1), ('l', 2)] for word = 'hello'
     '''
+    word = word.lower()
+    letters_of_word = list(set([element for element in word]))
+    num_of_repetition = []
+    for letter in letters_of_word:
+        num_of_repetition.append(word.count(letter))
+
+    letter_repetition = []
+    for index in range(0,len(letters_of_word)):
+        letter_repetition.append((letters_of_word[index],num_of_repetition[index]))
+    
+    return letter_repetition
 
 
 def check_requirements(word, letters):
     '''
     Check is the word follow all requirements.
     '''
+    num_of_letters = get_num_of_letters(word)
+    if (len(word) < 4) or (letters[4] not in word):
+        return False
+    letters_str = ''
+    for letter in letters:
+        letters_str += letter
+    for pair in num_of_letters:
+        if (letters_str.count(pair[0])) < pair[1]:
+            return False
+    
+    return True
 
 
 def generate_grid() -> List[List[str]]:
